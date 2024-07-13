59a4b4df00456d5007eac244fd7d9e4dd5f46ce5
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-07-13 08:02:39 +0300

    Add validate_input() function

--- utils.cpp
+++ utils.cpp
@@ -1,9 +1,18 @@
 #include "utils.hpp"
 
 
-bool validate_input(int packets, int* modules)
+int validate_input(int packets, int* modules)
 {
-
+    if( packets <= 0 )
+        // "Invalid packets number, packets number must be postive"
+        return 1;
+    for(int i=0; i<packets; ++i) 
+    {
+        if(modules[i] <= 0)
+            // "Invalid module number, all module number must be positive"
+            return 2;
+    }
+    return 0;  // "Valid input"
 }
 void first_last_modules(int &first, int &last, int n, int* modules)
 {
@@ -15,5 +24,5 @@ int* assign_valid_invalid(int n, int* modules)
 }
 void csv_output(int n, int* modules, int* validModule)
 {
-    
+
 }
--- utils.hpp
+++ utils.hpp
@@ -1,7 +1,7 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-bool validate_input(int packets, int* modules);
+int validate_input(int packets, int* modules);
 void first_last_modules(int &first, int &last, int n, int* modules);
 int* assign_valid_invalid(int n, int* modules);
 void csv_output(int n, int* modules, int* validModule);
