5d248c16bc415d2edace194bd251bbdf611a8c47
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-07-13 12:56:55 +0300

    Refactor main funcion

--- main.cpp
+++ main.cpp
@@ -1,41 +1,30 @@
 #include "utils.hpp"
-using namespace std;
 
 int main(int argc, char** argv) 
 {
+    int check = validate_input(argv);
+    if(check)
+    {
+        cout<<"Invalid inputs, all inputs must be positive numbers"<<'\n';
+        return 1;
+    }
     int packets = stoi(argv[1]);
-    while(packets<=0) 
-    {
-        cout<<"Invalid packets number, packets number must be postive"<<'\n';
-        return 1;
-    }
     int* modules = convert_argv_to_int_array(argc, argv);
-    int check = validate_input(packets, modules);
-    if(check==1)
+    if (modules == nullptr)
     {
-        cout<<"Invalid module number, all module number must be positive"<<'\n';
+        cout<<"No modules provided, please provide modules"<<'\n';
         return 1;
     }
-    else
+    int counter = 1;
+    remove("output.csv"); // remove prev output file
+    do
     {
-        cout<<"Valid inputs"<<'\n';
-        int counter = 1;
-        if (std::remove("output.csv") != 0) 
-        {
-            std::cerr << "Error deleting the file: " << "output.csv" << std::endl;
-        } 
-        else 
-        {
-            std::cout << "File successfully deleted: " << "output.csv" << std::endl;
-        }
-        do {
-                int first, last;
-                first_last_modules(first, last, packets, modules);
-                int* validModules =  assign_valid_invalid(first, last, packets, modules);
-                csv_output(counter++, packets, modules, validModules);
-                delete[] validModules;
-        } while (std::next_permutation(modules, modules + packets));
-    }
+        int first, last;
+        first_last_modules(first, last, packets, modules);
+        int* validModules =  assign_valid_invalid(first, last, packets, modules);
+        csv_output(counter++, packets, modules, validModules);
+        delete[] validModules;
+    } while (next_permutation(modules, modules + packets));
     return 0;
     
 }
